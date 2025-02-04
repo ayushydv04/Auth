6ee26dabc55b2c9219443aef42c4629028b30040
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2025-02-04 13:08:54 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+// dotenv configuration requiring
+require("dotenv").config()
 
-app.get("/", function(req, res) {})
+// DB connection requireing and running
+const connectToDb = require("./config/mongoose-connection");
+const cookieParser = require("cookie-parser"); // requiring cookie parser
+
+
+// middleware requireing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+connectToDb()   // running connect-to-db to connect to the database
+
+
+app.get("/", function(req, res) {
+    res.send("Hello World!");
+})
 
 
 app.listen(3000, function() {
---
+++ models/user-model.js
@@ -0,0 +1,77 @@
+// requiring mongoose firstly
+const mongoose = require("mongoose");
+
+
+// then next is creating schema
+
+const userSchema = mongoose.Schema({
+    name : {type: 'string', required: true},
+    email: {type: 'string', required: true, unique: true},
+    password: {type: 'string', required: true}
+})
+
+
+// Later in this we will use middleware in this which is .presave() like this, usse aap koi bhi data save krne se pehle kuch kaam perform kr skte ho, pre method hai userSchema mei aur pre ke andr hm save se pehle kuch kaam krwa skte h, mtlb ek naya data create hone se pehle ya kuch save krne se pehle hm kuch kaam krwa skte h
+
+
+module.exports = mongoose.model("user", userSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
